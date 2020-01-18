196a1372be518b7e09a4e39446b5c13e62c260e5
Daniel Joji Koons <jojikoons@gmail.com>
2020-01-18 16:04:39 -0700

    No css yet.

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Cool Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Nasty Burger", False);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Darn good Burger", TRUE);
+
