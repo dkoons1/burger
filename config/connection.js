f0cbdd4b5eb477e011758bfbb6cdb54d3b5a29aa
Daniel Joji Koons <jojikoons@gmail.com>
2020-01-18 16:59:17 -0700

    more heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,24 @@
 var mysql = require("mysql");
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "joji",
-    password: "trilogy1",
-    database: "burgers_db"
-  });
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "joji",
+//     password: "trilogy1",
+//     database: "burgers_db"
+//   });
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'joji',
+    password: 'trilogy1',
+    database: 'burgers_db'
+  })
+}
 
   connection.connect(function(err) {
     if (err) {
